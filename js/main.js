db3b32e10bc76c8f8e2da4aeadf9215f4b8f7b4a
helping083 <helping083@gmail.com>
2021-10-19 23:26:49 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$(function() {
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: `<button class="banner-section__slider-btn banner-section__slider-btnprev">
+    <img src="images/heart.svg">
+  </button>`,
+    nextArrow: `<button class="banner-section__slider-btn banner-section__slider-btnnext">
+    <img src="images/heart.svg">
+  </button>`
+  });
 });
